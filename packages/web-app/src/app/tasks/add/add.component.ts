78c6f4a37149c5e40f8defe6aa32ff459e452313
yanliu1111 <liuyan0504@gmail.com>
2025-02-21 00:53:57 -0700

    feat: add task

--- packages/web-app/src/app/tasks/add/add.component.ts
+++ packages/web-app/src/app/tasks/add/add.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Task, TaskPriority } from '@take-home/shared';
+
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
 import { StorageService } from '../../storage/storage.service';
 import { faker } from '@faker-js/faker';
 
 @Component({
-    selector: 'take-home-add-component',
-    templateUrl: './add.component.html',
-    styleUrls: ['./add.component.scss'],
-    standalone: false
+  selector: 'take-home-add-component',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule, // Add CommonModule to imports
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatOptionModule,
+  ],
 })
 export class AddComponent {
   protected addTaskForm: FormGroup = new FormGroup({
     title: new FormControl(null, {
-      // TODO: add validators for required and min length 10
+      validators: [Validators.required, Validators.minLength(10)],
     }),
     description: new FormControl(null),
     priority: new FormControl(
@@ -28,22 +49,21 @@ export class AddComponent {
 
   constructor(private storageService: StorageService, private router: Router) {}
 
-  onSubmit() {
-    const newTask: Task = {
-      ...this.addTaskForm.getRawValue(),
-      uuid: faker.string.uuid(),
-      isArchived: false,
-      // TODO: allow user to set scheduled date using MatDatePicker
-      scheduledDate: new Date(),
-    };
+  async onSubmit() {
+    if (this.addTaskForm.valid) {
+      const newTask: Task = {
+        ...this.addTaskForm.getRawValue(),
+        uuid: faker.string.uuid(),
+        isArchived: false,
+        scheduledDate: new Date(),
+      };
 
-    // TODO: save updated task to storage
-    // TODO: navigate to home page
-    throw new Error('Not implemented');
+      await this.storageService.addTaskItem(newTask);
+      this.router.navigate(['/']);
+    }
   }
 
   onCancel(): void {
-    // TODO: navigate to home page
-    throw new Error('Not implemented');
+    this.router.navigate(['/']);
   }
 }
